979ca42cda80b58137b738890e22ebdbf5dbfa3f
ashinoff <ashinoff@gmail.com>
2025-07-03 17:41:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from handlers.line_staff import handler_start, handler_text
 # Создаём Telegram Application
 application = ApplicationBuilder().token(TOKEN).build()
 
-# Загружаем данные
+# Загружаем данные в бот
 from config import BRANCH_URLS, VISIBILITY_GROUPS
 application.bot_data["branches"] = list(BRANCH_URLS.keys())
 application.bot_data["visibility_groups"] = VISIBILITY_GROUPS
@@ -23,7 +23,9 @@ application.add_handler(handler_start)
 application.add_handler(handler_text)
 
 if __name__ == '__main__':
+    # Пинги для Render
     threading.Thread(target=ping_self, daemon=True).start()
+    # Запускаем встроенный PTB webhook-сервер
     application.run_webhook(
         listen="0.0.0.0",
         port=PORT,
