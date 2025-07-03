a15c0dd64dfa7693e3ba992b03829c907e35c5b9
ashinoff <ashinoff@gmail.com>
2025-07-03 17:48:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,25 @@ from telegram.ext import ApplicationBuilder
 from config import TOKEN, SELF_URL, PORT
 from utils import ping_self
 
-# Хендлеры
-from handlers.admin import handler as admin_start
-from handlers.director import handler as director_start
+# Единый хендлер интерфейса
 from handlers.line_staff import handler_start, handler_text
 
-# Создаём Telegram Application
 application = ApplicationBuilder().token(TOKEN).build()
 
-# Загружаем данные в бот
+# Загружаем конфиги
 from config import BRANCH_URLS, VISIBILITY_GROUPS
-application.bot_data["branches"] = list(BRANCH_URLS.keys())
-application.bot_data["visibility_groups"] = VISIBILITY_GROUPS
+application.bot_data['branches'] = list(BRANCH_URLS.keys())
+application.bot_data['visibility_groups'] = VISIBILITY_GROUPS
 
-# Регистрируем хендлеры
-application.add_handler(admin_start)
-application.add_handler(director_start)
+# Регистрируем только два хендлера
 application.add_handler(handler_start)
 application.add_handler(handler_text)
 
 if __name__ == '__main__':
-    # Пинги для Render
     threading.Thread(target=ping_self, daemon=True).start()
-    # Запускаем встроенный PTB webhook-сервер
     application.run_webhook(
-        listen="0.0.0.0",
+        listen='0.0.0.0',
         port=PORT,
-        url_path="webhook",
-        webhook_url=f"{SELF_URL}/webhook"
+        url_path='webhook',
+        webhook_url=f'{SELF_URL}/webhook'
     )
