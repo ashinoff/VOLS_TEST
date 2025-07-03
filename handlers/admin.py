609055189134d28c81c0b68aa7518b03627cb142
ashinoff <ashinoff@gmail.com>
2025-07-03 17:43:01 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,20 +3,23 @@ from telegram.ext import ContextTypes, CommandHandler
 from config import VISIBILITY_GROUPS
 
 # Задайте реальные ID администраторов
-ADMINS = {955536270, 987654321}
+ADMINS = {123456789, 987654321}
 
-# Клавиатура выбора группы видимости
-kb_visibility = ReplyKeyboardMarkup(
-    [[name] for name in VISIBILITY_GROUPS.keys()],
-    resize_keyboard=True
-)
+# Начальное меню с выбором группы, телефонов провайдеров и отчета
+initial_buttons = [
+    ["Россети ЮГ"],
+    ["Россети Кубань"],
+    ["Телефоны провайдеров"],
+    ["Сформировать отчет"],
+]
+kb_initial = ReplyKeyboardMarkup(initial_buttons, resize_keyboard=True)
 
 async def start_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id not in ADMINS:
         return
     await update.message.reply_text(
-        "👑 Здравствуйте, администратор! Выберите группу филиалов:",
-        reply_markup=kb_visibility
+        "👑 Здравствуйте, администратор! Выберите опцию:",
+        reply_markup=kb_initial
     )
 
-handler = CommandHandler("start", start_admin)
+handler = CommandHandler("start", start_admin)("start", start_admin)
