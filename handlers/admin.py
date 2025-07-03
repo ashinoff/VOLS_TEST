0b989d7378a762158846bef3e9374b714d9955e0
ashinoff <ashinoff@gmail.com>
2025-07-03 17:20:03 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,12 +1,22 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes, CommandHandler
+from config import VISIBILITY_GROUPS
 
-# Здесь укажите реальные ID админов
-ADMINS = {955536270, 987654321}
+# ID админов
+ADMINS = {123456789, 987654321}
+
+# клавиатура выбора RK/RU
+kb_visibility = ReplyKeyboardMarkup(
+    [[k] for k in VISIBILITY_GROUPS.keys()],
+    resize_keyboard=True
+)
 
 async def start_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id not in ADMINS:
         return
-    await update.message.reply_text("👑 Здравствуйте, администратор!")
+    await update.message.reply_text(
+        "👑 Здравствуйте, администратор! Выберите группу филиалов:",
+        reply_markup=kb_visibility
+    )
 
 handler = CommandHandler("start", start_admin)
