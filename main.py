541b575a1011b327b8fd0d23b0f516681643bb81
ashinoff <ashinoff@gmail.com>
2025-07-03 17:27:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from handlers.director import handler as director_start
 from handlers.line_staff import handler_start, handler_text
 
 # Создаём Telegram Application
-application = ApplicationBuilder() \
-    .token(TOKEN) \
-    .build()
+application = ApplicationBuilder().token(TOKEN).build()
 
 # Загружаем данные
 from config import BRANCH_URLS, VISIBILITY_GROUPS
@@ -25,9 +23,7 @@ application.add_handler(handler_start)
 application.add_handler(handler_text)
 
 if __name__ == '__main__':
-    # Пинги для Render
     threading.Thread(target=ping_self, daemon=True).start()
-    # Запускаем PTB webhook
     application.run_webhook(
         listen="0.0.0.0",
         port=PORT,
