33f75a04b755cd9d8fb0c27bd44b51fcb3767ff7
ashinoff <ashinoff@gmail.com>
2025-07-03 11:43:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+import os
+
+TOKEN         = os.getenv("TOKEN")
+SELF_URL      = os.getenv("SELF_URL", "").rstrip('/')
+ZONES_CSV_URL = os.getenv("ZONES_CSV_URL", "").strip()
+
+BRANCH_URLS = {
+    "Юго-Западные ЭС": os.getenv("YUGO_ZAPAD_ES_URL", ""),
+    "Усть-Лабинские ЭС": os.getenv("UST_LAB_ES_URL", ""),
+    "Тимашевские ЭС":    os.getenv("TIMASHEV_ES_URL", ""),
+    "Тихорецкие ЭС":     os.getenv("TIKHORETS_ES_URL", ""),
+    "Сочинские ЭС":      os.getenv("SOCH_ES_URL", ""),
+    "Славянские ЭС":     os.getenv("SLAV_ES_URL", ""),
+    "Ленинградские ЭС":  os.getenv("LENINGRAD_ES_URL", ""),
+    "Лабинские ЭС":      os.getenv("LABIN_ES_URL", ""),
+    "Краснодарские ЭС":  os.getenv("KRASN_ES_URL", ""),
+    "Армавирские ЭС":    os.getenv("ARMAVIR_ES_URL", ""),
+    "Адыгейские ЭС":     os.getenv("ADYGEA_ES_URL", ""),
+}
+BRANCHES = list(BRANCH_URLS.keys())
