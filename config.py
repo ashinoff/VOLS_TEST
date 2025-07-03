88806d4dec7a0da8ecc0416a2b97c370df050807
ashinoff <ashinoff@gmail.com>
2025-07-04 00:01:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# config.py
+
 import os
 
 # Токен бота
@@ -6,18 +8,17 @@ TOKEN = os.getenv("TOKEN")
 # Публичный URL приложения без конечного слэша
 SELF_URL = os.getenv("SELF_URL", "").rstrip('/')
 
-# Порт (Render задаёт автоматически)
-# По умолчанию 5000
+# Порт (Render задаёт автоматически), по умолчанию 5000
 PORT = int(os.getenv("PORT", 5000))
 
-# Ссылка на Google Sheet с колонками:
+# Google Sheet с колонками:
 # Видимость | Филиал | РЭС | ID | ФИО | Ответственный
 ZONES_CSV_URL = os.getenv("ZONES_CSV_URL", "").strip()
 
-# Отдельный лист для уведомлений по ТП/ВЛ
+# Отдельный лист для поиска при уведомлениях (ТП/ВЛ)
 NOTIFY_SHEET_URL = os.getenv("TIMASHEV_ES_URL_SP", "").strip()
 
-# CSV URL-ы филиальных таблиц (ВОЛС)
+# CSV URL-ы филиальных табличек
 BRANCH_URLS = {
     "Юго-Западные ЭС":   os.getenv("YUGO_ZAPAD_ES_URL", ""),
     "Усть-Лабинские ЭС":  os.getenv("UST_LAB_ES_URL", ""),
@@ -32,7 +33,7 @@ BRANCH_URLS = {
     "Адыгейские ЭС":      os.getenv("ADYGEA_ES_URL", ""),
 }
 
-# Разбиение филиалов по видимости
+# Разбиение филиалов по видам сетей
 VISIBILITY_GROUPS = {
     "Россети ЮГ": [
         "Юго-Западные ЭС",
